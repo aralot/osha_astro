6131b580485ad33e407c35694be5fc60fdaaddb0
Sergey Zhirkov <sergey.zhirkov@alg.team>
2023-08-30 17:06:10 +0300

    Показываем видео ютуба только в момент, когда оно в поле зрения

--- src/components/courses/projectAndCertificates/project/Project.tsx
+++ src/components/courses/projectAndCertificates/project/Project.tsx
@@ -1,30 +1,63 @@
-import React, { FunctionComponent, useCallback, useRef } from 'react';
+import React, {
+  FunctionComponent,
+  useCallback,
+  useEffect,
+  useRef,
+} from 'react';
 
 import { Wrapper, Iframe, Placeholder } from './styles';
 
 import placeholder from './placeholder.webp';
 
 const IFRAME_SRC =
-  'https://www.youtube.com/embed/yc3HqFUtp4I?&amp;loop=1&amp;playlist=yc3HqFUtp4I&amp;mute=1&amp;autoplay=1';
+  'https://www.youtube.com/embed/yc3HqFUtp4I?loop=1&playlist=yc3HqFUtp4I&mute=1&autoplay=1&enablejsapi=1';
 
 const Project: FunctionComponent = ({}) => {
   const iframeRef = useRef(null);
+  const wrapperRef = useRef(null);
 
-  // const onLoad = useCallback(() => {
-  //   if (iframeRef.current) {
-  //     setTimeout(() => {
-  //       iframeRef.current.onload = event => {
-  //         event.target.style.opacity = 1;
-  //       };
-  //       iframeRef.current.src = IFRAME_SRC;
-  //     }, 5000);
-  //   }
-  // }, [iframeRef]);
+  useEffect(() => {
+    if (!wrapperRef.current || !iframeRef.current) return;
+
+    let player = null;
+
+    window.onYouTubeIframeAPIReady = () => {
+      player = new YT.Player('player');
+    };
+
+    const observer = new IntersectionObserver(
+      entry => {
+        const isIntersecting = entry[0].isIntersecting;
+
+        // player is initialized - toogle "play" / "pause"
+        if (player) {
+          player[isIntersecting ? 'playVideo' : 'pauseVideo']();
+          return;
+        }
+
+        if (isIntersecting) {
+          // already loading iframe
+          if (iframeRef.current.src) return;
+
+          // load iframe
+          iframeRef.current.onload = event => {
+            event.target.style.opacity = 1;
+          };
+          iframeRef.current.src = IFRAME_SRC;
+        }
+      },
+      {
+        root: null,
+        threshold: 0.75,
+      },
+    );
+    observer.observe(wrapperRef.current);
+  }, []);
 
   return (
-    <Wrapper>
+    <Wrapper ref={wrapperRef}>
       <Placeholder src={placeholder} loading="lazy" />
-      <Iframe ref={iframeRef} />
+      <Iframe id="player" ref={iframeRef} />
     </Wrapper>
   );
 };
