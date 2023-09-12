555b262d78c2adeefc55feb71eb9e7a6f045cabe
Sergey Zhirkov <sergey.zhirkov@alg.team>
2023-09-12 16:05:01 +0300

    Убрал остатки partytown

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,6 @@
 import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
 
-import partytown from "@astrojs/partytown";
-
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()]
